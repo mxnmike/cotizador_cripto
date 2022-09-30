b8e345c60e3ed09427020bee75d1f733dac9b0a0
MXNMike <mxnmike@me.com>
2022-09-29 17:17:05 -0700

    Proyecto terminado

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,55 @@
+import styled from '@emotion/styled'
+
+const Contenedor = styled.div`
+  color: #fff;
+  font-family: 'Lato', sans-serif;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 30px;
+`
+
+const Texto = styled.p`
+  font-size: 18px;
+  span {
+    font-weight: 700;
+  }
+`
+const Precio = styled.p`
+  font-size: 24px;
+  span {
+    font-weight: 700;
+  }
+`
+const Imagen = styled.img`
+  width: 120px;
+`
+
+const Resultado = ({ cotizacion }) => {
+  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } =
+    cotizacion
+  return (
+    <Contenedor>
+      <Imagen src={`https://cryptocompare.com/${IMAGEURL}`} />
+      <div>
+        <Precio>
+          El Precio es de: <span>{PRICE}</span>
+        </Precio>
+        <Texto>
+          El Precio más alto del día: <span>{HIGHDAY}</span>
+        </Texto>
+        <Texto>
+          El Precio más bajo del día: <span>{LOWDAY}</span>
+        </Texto>
+        <Texto>
+          Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span>
+        </Texto>
+        <Texto>
+          Última actualización: <span>{LASTUPDATE}</span>
+        </Texto>
+      </div>
+    </Contenedor>
+  )
+}
+
+export default Resultado
